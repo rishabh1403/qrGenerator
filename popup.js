b624f121e8893e0584b387544e7b10857570d8f0
Rishabh Jain <mailrishabh1403@gmail.com>
2016-10-26 00:52:48 +0530

    generating link

---
+++ popup.js
@@ -0,0 +1,14 @@
+var tabQuery = {
+    "active" : true,
+    "currentWindow" : true
+};
+
+chrome.tabs.query(tabQuery,function(tabs){
+    var url = tabs[0].url;
+    console.log(url);
+    var qrcode = new QRCode(document.getElementById("qrcode"), {
+      width: 300,
+      height: 300
+    });
+    qrcode.makeCode(url);
+});
